8c0275652d0345822dbea7d906fc641779b02bb4
RexshackGaming <rexshackuk@gmail.com>
2023-12-12 12:57:35 +0000

    preserve stamina leaving bath house

--- client/client.lua
+++ client/client.lua
@@ -378,9 +378,12 @@ end
 
 DressCharacter = function()
     local currentHealth = GetEntityHealth(PlayerPedId())
+    local maxStamina = Citizen.InvokeNative(0xCB42AFE2B613EE55, PlayerPedId(), Citizen.ResultAsFloat())
+    local currentStamina = Citizen.InvokeNative(0x775A1CA7893AA8B5, PlayerPedId(), Citizen.ResultAsFloat()) / maxStamina * 100
     TriggerServerEvent('rsg-appearance:LoadSkin')
     Wait(1000)
     SetEntityHealth(PlayerPedId(), currentHealth )
+    Citizen.InvokeNative(0xC3D4B754C0E86B9E, PlayerPedId(), currentStamina)
 end
 
 SetCurrentCleaniest = function(rag, value)
